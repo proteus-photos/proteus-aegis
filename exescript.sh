8d3e83a1a63adf92971ac9b1590b187c52f25d3d
lovely-necromancer <lovely_necromancer@proton.me>
2024-09-10 13:00:26 +0200

    add exe script

---
+++ exescript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if the user provided an argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <number_of_instances>"
+    exit 1
+fi
+
+# Get the number of parallel instances to run
+n="$1"
+
+# Run n parallel instances of proteusBool
+for ((i=1; i<=n; i++)); do
+    echo "Starting instance $i"
+    proteusBool &  # Run the instance in the background
+done
+
+# Wait for all parallel instances to finish
+wait
+
+echo "All $n instances of proteusBool have finished."
